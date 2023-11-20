aba2af068fb29d6b5dc3e721e701d5ae7b831a75
Arin Mehta <arinmehta@Arins-MacBook-Pro.local>
2023-11-20 16:47:10 -0600

    in progress

--- src/main/rice/Main.java
+++ src/main/rice/Main.java
@@ -1,3 +1,33 @@
 package main.rice;
 
+import main.rice.basegen.BaseSetGenerator;
+import main.rice.concisegen.ConciseSetGenerator;
+import main.rice.parse.ConfigFile;
+import main.rice.parse.InvalidConfigException;
+import main.rice.test.TestCase;
+import main.rice.test.TestResults;
+import main.rice.test.Tester;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
 // TODO: implement the Main class here
+public class Main{
+    public static void main(String[] args) throws IOException, InvalidConfigException, InterruptedException{
+        Set<TestCase> toPrint = generateTests(args);
+        System.out.println("Prints a set of test cases that is an approximately minimal set covering");
+        for (TestCase testCase: toPrint){
+            System.out.println(testCase.toString());
+        }
+    }
+    public static Set<TestCase> generateTests(String[] args) throws IOException, InvalidConfigException, InterruptedException{
+        ConfigFile configFile = ConfigFileParser(args[0]);
+        BaseSetGenerator baseSetGenerator = new BaseSetGenerator(configFile.getNodes(), configFile.getNumRand());
+        Tester tester = new Tester(configFile.getFuncName(), args[1], args[2],baseSetGenerator.genBaseSet());
+        tester.computeExpectedResults();
+        TestResults testResults = tester.runTests();
+        return ConciseSetGenerator.setCover(testResults);
+    }
+}
--- src/main/rice/parse/ConfigFileParser.java
+++ src/main/rice/parse/ConfigFileParser.java
@@ -1,3 +1,30 @@
 package main.rice.parse;
 
-// TODO: implement the ConfigFileParser class here
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ConfigFileParser{
+    public static String readFile(String filepath) throws IOException {
+        Reader reader = new FileReader();
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null){
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private void JSONObjKeyValueCheck(JSONObject jsonObj) throws InvalidConfigException{
+        try{jsonObj.get("fname");
+        jsonObj.get("types");
+        jsonObj.get("exhaustive domain");
+        jsonObj.get("random domain");
+        jsonObj.get("num random")}
+        catch()
+    }
+}
